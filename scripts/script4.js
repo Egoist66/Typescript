6c9c7747940d130a83af69fb97f10d76030984af
Farid Mahmudov <Razormad666@gmail.com>
2024-02-13 00:09:31 +0300

    update

--- scripts/script4.js
+++ scripts/script4.js
@@ -12,3 +12,35 @@ function fetchData(data) {
     }
 }
 fetchData('farid');
+var userData = '{"name": "Farid", "age": "26", "birthDay": "12/07/1997"}';
+function safeParse(str) {
+    return JSON.parse(str);
+}
+var data = safeParse(userData);
+function transferData(d) {
+    if (typeof d === 'string') {
+        console.log(d.toLocaleLowerCase());
+    }
+    else if (typeof d === 'object' && d) {
+        console.log(d);
+    }
+    else {
+        console.log('Error');
+    }
+}
+transferData(data);
+var Singleton = /** @class */ (function () {
+    function Singleton() {
+        if (!Singleton.instance) {
+            // Логика инициализации экземпляра
+            this.name = "Экземпляр синглтона";
+            Singleton.instance = this;
+        }
+        return Singleton.instance;
+    }
+    return Singleton;
+}());
+// Использование синглтона
+var singletonInstance1 = new Singleton();
+var singletonInstance2 = new Singleton();
+console.log(singletonInstance1 === singletonInstance2); // Выводит true
--- scripts/script4.ts
+++ scripts/script4.ts
@@ -19,3 +19,49 @@ function fetchData(data: unknown): void {
 }
 
 fetchData('farid')
+
+
+const userData= '{"name": "Farid", "age": "26", "birthDay": "12/07/1997"}'
+
+function safeParse(str: string): unknown {
+    return JSON.parse(str)
+}
+
+const data = safeParse(userData)
+
+function transferData(d: unknown): void {
+    if(typeof d === 'string'){
+        console.log(d.toLocaleLowerCase());
+    }
+    else if(typeof d === 'object' && d){
+        console.log(d);
+    }
+    else {
+        console.log('Error')
+    }
+}
+
+transferData(data)
+
+
+class Singleton {
+    static instance: Singleton;
+    name!: string;
+
+    constructor() {
+      if (!Singleton.instance) {
+        // Логика инициализации экземпляра
+        this.name = "Экземпляр синглтона";
+        Singleton.instance = this;
+      }
+      return Singleton.instance;
+    }
+  }
+
+  
+  
+  // Использование синглтона
+  const singletonInstance1 = new Singleton();
+  const singletonInstance2 = new Singleton();
+  
+  console.log(singletonInstance1 === singletonInstance2); // Выводит true
