3ea4aad021274ed5ded9c6899102fa24730f5280
Farid Mahmudov <Razormad666@gmail.com>
2024-03-05 15:15:46 +0300

    update

--- scripts/script5.js
+++ scripts/script5.js
@@ -1,66 +1,123 @@
-// Перечисление с названием TypesOfMedia, которое включает строчные типы video, audio
-// Перечисление с названием FormatsOfMedia, которое включает строчные видео-форматы: .mp4, .mov, .mkv, .flv, .webM
-// Описание интерфейса, в котором:
-// name - строка
-// type - один из перечисления выше
-// format = один из перечисления выше
-// subtitles - необязательное поле типа строка
-// marks - необязательное поле неизвестного типа
-var TypesOfMedia;
-(function (TypesOfMedia) {
-    TypesOfMedia["video"] = "video";
-    TypesOfMedia["audio"] = "audio";
-})(TypesOfMedia || (TypesOfMedia = {}));
-var FormatsOfMedia;
-(function (FormatsOfMedia) {
-    FormatsOfMedia["mp4"] = ".mp4";
-    FormatsOfMedia["mov"] = ".mov";
-    FormatsOfMedia["mkv"] = ".mkv";
-    FormatsOfMedia["flv"] = ".flv";
-    FormatsOfMedia["webM"] = ".webM";
-})(FormatsOfMedia || (FormatsOfMedia = {}));
-function playMedia(_a) {
-    var _b = _a === void 0 ? {
-        name: "example",
-        type: TypesOfMedia.audio,
-        format: FormatsOfMedia.mp4,
-    } : _a, name = _b.name, type = _b.type, format = _b.format, subtitles = _b.subtitles, marks = _b.marks;
-    var marksLog = '';
-    // Создать функционал, что если marks - это массив, то "сложить" все эелементы в одну строку и поместить в marksLog
-    // Если это строка, то просто поместить её в marksLog
-    // Если что-то другое - то marksLog = "Unsupported type of marks"
-    // Не допускайте any!
-    if (Array.isArray(marks)) {
-        marksLog = marks.join(" ");
+//
+// // Перечисление с названием TypesOfMedia, которое включает строчные типы video, audio
+//
+// // Перечисление с названием FormatsOfMedia, которое включает строчные видео-форматы: .mp4, .mov, .mkv, .flv, .webM
+//
+// // Описание интерфейса, в котором:
+// // name - строка
+// // type - один из перечисления выше
+// // format = один из перечисления выше
+// // subtitles - необязательное поле типа строка
+// // marks - необязательное поле неизвестного типа
+//
+//
+// enum TypesOfMedia  {
+//     video = "video",
+//     audio = "audio"
+// }
+//
+// enum FormatsOfMedia {
+//     mp4 = ".mp4",
+//     mov = ".mov",
+//     mkv = ".mkv",
+//     flv = ".flv",
+//     webM = ".webM"
+// }
+//
+// interface IPlayMedia {
+//     name: string,
+//     type: TypesOfMedia,
+//     format: FormatsOfMedia,
+//     subtitles?: string,
+//     marks?: unknown
+// }
+//
+// function playMedia(
+// 	{ name, type, format, subtitles, marks }: IPlayMedia = {
+// 		name: "example",
+// 		type: TypesOfMedia.audio,
+// 		format: FormatsOfMedia.mp4,
+// 	}
+// ): string {
+// 	let marksLog: string = '';
+//
+//     // Создать функционал, что если marks - это массив, то "сложить" все эелементы в одну строку и поместить в marksLog
+//     // Если это строка, то просто поместить её в marksLog
+//     // Если что-то другое - то marksLog = "Unsupported type of marks"
+//     // Не допускайте any!
+//
+//     if(Array.isArray(marks)){
+//         marksLog = marks.join(" ")
+//     }
+//     else if(typeof marks === 'string'){
+//         marksLog = marks
+//     }
+//     else {
+//         marksLog = 'Unsupported type of marks'
+//     }
+//
+// 	console.log(`Media ${name}${format} is ${type}
+//     Marks: ${marksLog}
+//     Subtitles: ${subtitles ?? "none"}`);
+//     // помните что это за оператор ??
+//
+// 	return "Media started";
+// }
+//
+// // playMedia({
+// // 	name: "WoW",
+// // 	format: FormatsOfMedia.mp4,
+// // 	type: TypesOfMedia.audio,
+// // 	subtitles: "hmhmhm hmhmhm doh",
+// // 	marks: ["4:30", "5:40"],
+// // });
+//
+//
+//
+//
+// const loadData = (url: string, method: "GET" | "POST"): void => {
+//     console.log('Fetched!');
+//
+// }
+//
+// const requestOption = {
+//     url: 'https://someurl.com',
+//     method: 'GET'
+// } as const
+//
+// loadData('qqq', 'GET')
+// loadData(requestOption.url, requestOption.method)
+//
+// // loadData(requestOption.url, <"GET">requestOption.method)
+//
+//
+// const box = document.querySelector('.box') as HTMLElement
+// box.style.cssText = 'background: skyblue; margin: 10px 0px'
+//
+// const input = document.querySelector('input') as HTMLInputElement
+//
+// const someNumber: number = parseInt(input.value)
+function is(entity, matchType) {
+    return typeof entity === matchType;
+}
+function isNumber(n) {
+    return typeof n === "number";
+}
+function repairVehicle(vehicle) {
+    if (isCar(vehicle)) {
+        console.log(vehicle.wheels);
     }
-    else if (typeof marks === 'string') {
-        marksLog = marks;
+    else if (isShip(vehicle)) {
+        console.log(vehicle.sail);
     }
     else {
-        marksLog = 'Unsupported type of marks';
+        vehicle;
     }
-    console.log("Media ".concat(name).concat(format, " is ").concat(type, "\n    Marks: ").concat(marksLog, "\n    Subtitles: ").concat(subtitles !== null && subtitles !== void 0 ? subtitles : "none"));
-    // помните что это за оператор ??
-    return "Media started";
 }
-// playMedia({
-// 	name: "WoW",
-// 	format: FormatsOfMedia.mp4,
-// 	type: TypesOfMedia.audio,
-// 	subtitles: "hmhmhm hmhmhm doh",
-// 	marks: ["4:30", "5:40"],
-// });
-var loadData = function (url, method) {
-    console.log('Fetched!');
-};
-var requestOption = {
-    url: 'https://someurl.com',
-    method: 'GET'
-};
-loadData('qqq', 'GET');
-loadData(requestOption.url, requestOption.method);
-// loadData(requestOption.url, <"GET">requestOption.method)
-var box = document.querySelector('.box');
-box.style.cssText = 'background: skyblue; margin: 10px 0px';
-var input = document.querySelector('input');
-var someNumber = parseInt(input.value);
+repairVehicle({ wheels: '3', engine: 'db900' });
+function isCar(vehicle) {
+    return 'wheels' in vehicle;
+}
+function isShip(vehicle) {
+    return 'sail' in vehicle;
+}
--- scripts/script5.ts
+++ scripts/script5.ts
@@ -1,99 +1,155 @@
+//
+// // Перечисление с названием TypesOfMedia, которое включает строчные типы video, audio
+//
+// // Перечисление с названием FormatsOfMedia, которое включает строчные видео-форматы: .mp4, .mov, .mkv, .flv, .webM
+//
+// // Описание интерфейса, в котором:
+// // name - строка
+// // type - один из перечисления выше
+// // format = один из перечисления выше
+// // subtitles - необязательное поле типа строка
+// // marks - необязательное поле неизвестного типа
+//
+//
+// enum TypesOfMedia  {
+//     video = "video",
+//     audio = "audio"
+// }
+//
+// enum FormatsOfMedia {
+//     mp4 = ".mp4",
+//     mov = ".mov",
+//     mkv = ".mkv",
+//     flv = ".flv",
+//     webM = ".webM"
+// }
+//
+// interface IPlayMedia {
+//     name: string,
+//     type: TypesOfMedia,
+//     format: FormatsOfMedia,
+//     subtitles?: string,
+//     marks?: unknown
+// }
+//
+// function playMedia(
+// 	{ name, type, format, subtitles, marks }: IPlayMedia = {
+// 		name: "example",
+// 		type: TypesOfMedia.audio,
+// 		format: FormatsOfMedia.mp4,
+// 	}
+// ): string {
+// 	let marksLog: string = '';
+//
+//     // Создать функционал, что если marks - это массив, то "сложить" все эелементы в одну строку и поместить в marksLog
+//     // Если это строка, то просто поместить её в marksLog
+//     // Если что-то другое - то marksLog = "Unsupported type of marks"
+//     // Не допускайте any!
+//
+//     if(Array.isArray(marks)){
+//         marksLog = marks.join(" ")
+//     }
+//     else if(typeof marks === 'string'){
+//         marksLog = marks
+//     }
+//     else {
+//         marksLog = 'Unsupported type of marks'
+//     }
+//
+// 	console.log(`Media ${name}${format} is ${type}
+//     Marks: ${marksLog}
+//     Subtitles: ${subtitles ?? "none"}`);
+//     // помните что это за оператор ??
+//
+// 	return "Media started";
+// }
+//
+// // playMedia({
+// // 	name: "WoW",
+// // 	format: FormatsOfMedia.mp4,
+// // 	type: TypesOfMedia.audio,
+// // 	subtitles: "hmhmhm hmhmhm doh",
+// // 	marks: ["4:30", "5:40"],
+// // });
+//
+//
+//
+//
+// const loadData = (url: string, method: "GET" | "POST"): void => {
+//     console.log('Fetched!');
+//
+// }
+//
+// const requestOption = {
+//     url: 'https://someurl.com',
+//     method: 'GET'
+// } as const
+//
+// loadData('qqq', 'GET')
+// loadData(requestOption.url, requestOption.method)
+//
+// // loadData(requestOption.url, <"GET">requestOption.method)
+//
+//
+// const box = document.querySelector('.box') as HTMLElement
+// box.style.cssText = 'background: skyblue; margin: 10px 0px'
+//
+// const input = document.querySelector('input') as HTMLInputElement
+//
+// const someNumber: number = parseInt(input.value)
 
-// Перечисление с названием TypesOfMedia, которое включает строчные типы video, audio
 
-// Перечисление с названием FormatsOfMedia, которое включает строчные видео-форматы: .mp4, .mov, .mkv, .flv, .webM
+// let num: Number = new Number(10);
+// let num2 = 10
+//
+// num = num2
+//
 
-// Описание интерфейса, в котором:
-// name - строка
-// type - один из перечисления выше
-// format = один из перечисления выше
-// subtitles - необязательное поле типа строка
-// marks - необязательное поле неизвестного типа
-
-
-enum TypesOfMedia  {
-    video = "video",
-    audio = "audio"
+type MatchType =
+    boolean
+    | number
+    | string
+    | object
+    | null
+    | undefined
+    | bigint
+    | symbol
+    | HTMLElement
+function is(entity: unknown, matchType: MatchType): entity is MatchType{
+    return typeof entity === matchType
+}
+function isNumber(n: number | string[] | boolean): n is number {
+    return  typeof n === "number"
 }
 
-enum FormatsOfMedia {
-    mp4 = ".mp4",
-    mov = ".mov",
-    mkv = ".mkv",
-    flv = ".flv",
-    webM = ".webM"
+interface Car {
+    engine: string
+    wheels: string
 }
 
-interface IPlayMedia {
-    name: string,
-    type: TypesOfMedia,
-    format: FormatsOfMedia,
-    subtitles?: string,
-    marks?: unknown
+interface Ship {
+    engine: string
+    sail: string
 }
 
-function playMedia(
-	{ name, type, format, subtitles, marks }: IPlayMedia = {
-		name: "example",
-		type: TypesOfMedia.audio,
-		format: FormatsOfMedia.mp4,
-	}
-): string {
-	let marksLog: string = '';
-
-    // Создать функционал, что если marks - это массив, то "сложить" все эелементы в одну строку и поместить в marksLog
-    // Если это строка, то просто поместить её в marksLog
-    // Если что-то другое - то marksLog = "Unsupported type of marks"
-    // Не допускайте any!
-
-    if(Array.isArray(marks)){
-        marksLog = marks.join(" ")
+function repairVehicle(vehicle: Car | Ship){
+    if(isCar(vehicle)){
+        console.log(vehicle.wheels)
     }
-    else if(typeof marks === 'string'){
-        marksLog = marks
+    else if(isShip(vehicle)) {
+        console.log(vehicle.sail)
     }
     else {
-        marksLog = 'Unsupported type of marks'
+        vehicle
     }
-
-	console.log(`Media ${name}${format} is ${type}
-    Marks: ${marksLog}
-    Subtitles: ${subtitles ?? "none"}`);
-    // помните что это за оператор ??
-
-	return "Media started";
 }
 
-// playMedia({
-// 	name: "WoW",
-// 	format: FormatsOfMedia.mp4,
-// 	type: TypesOfMedia.audio,
-// 	subtitles: "hmhmhm hmhmhm doh",
-// 	marks: ["4:30", "5:40"],
-// });
+repairVehicle({wheels: '3', engine: 'db900'})
 
-
-
-
-const loadData = (url: string, method: "GET" | "POST"): void => {
-    console.log('Fetched!');
-    
+function isCar(vehicle: Car | Ship): vehicle is Car{
+    return 'wheels' in vehicle
 }
 
-const requestOption = {
-    url: 'https://someurl.com',
-    method: 'GET'
-} as const
-
-loadData('qqq', 'GET')
-loadData(requestOption.url, requestOption.method)
-
-// loadData(requestOption.url, <"GET">requestOption.method)
-
-
-const box = document.querySelector('.box') as HTMLElement
-box.style.cssText = 'background: skyblue; margin: 10px 0px'
-
-const input = document.querySelector('input') as HTMLInputElement
-
-const someNumber: number = parseInt(input.value)
+function isShip(vehicle: Car | Ship): vehicle is Ship{
+    return 'sail' in vehicle
+}
